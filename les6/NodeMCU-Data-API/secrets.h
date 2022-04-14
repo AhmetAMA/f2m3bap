0ad4f1435680fd79f627c5794b811111d1453b9a
AhmetAMA <33016@ma-web.nl>
2022-04-14 09:20:53 +0200

    les6 toegevoegd

---
+++ les6/NodeMCU-Data-API/secrets.h
@@ -0,0 +1,11 @@
+
+#define WIFI_SSID "Medialab"
+#define WIFI_PASSWORD "Mediacollege2"
+
+
+
+
+#define SERVER_HOST "data.softwaredeveloper.amsterdam"
+#define SERVER_PORT 443
+#define DEVICE_KEY "a35d9187"
+#define DEVICE_SECRET "de4UvE6a"
